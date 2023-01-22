d2536daec96655d47b226ebf4c4b40191e218c15
KEVIN-DEVV <kyevenkims@gmail.com>
2023-01-22 10:43:20 +0300

    Completed assignement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { movies } from "../data";
 
-function Home() {
-  return <div>{/*{code here}*/}</div>;
+function Movies() {
+  const movieList=movies.map((movie)=>
+    <div key={movie.title}>
+      <h3>Name:{movie.title}</h3>
+      <p>Time:{movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        <li>{movie.genres[0]}</li>
+        <li>{movie.genres[1]}</li>
+        <li>{movie.genres[2]}</li>
+        <li>{movie.genres[3]}</li>
+        <li>{movie.genres[4]}</li>
+      </ul>
+
+    </div>
+    
+  )
+  return <div>
+    <h1>Home Page</h1>
+    {movieList}
+  </div>;
 }
 
-export default Home;
+export default Movies;
